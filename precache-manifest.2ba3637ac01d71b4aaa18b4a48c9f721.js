75a83f6efb4229d8bac3f730611eae5369869bf1
Kaung Wint Shein <k028176@gmail.com>
2021-11-22 13:13:01 +0630

    initial commit

---
+++ precache-manifest.2ba3637ac01d71b4aaa18b4a48c9f721.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb21d26b3fa9284ac81da427ac170f15",
+    "url": "/agency-gsap/index.html"
+  },
+  {
+    "revision": "9f2067ee611066541443",
+    "url": "/agency-gsap/static/css/main.5058b451.chunk.css"
+  },
+  {
+    "revision": "83dcc42f993251bb4b37",
+    "url": "/agency-gsap/static/js/2.95694cc6.chunk.js"
+  },
+  {
+    "revision": "baebdd1671010f55d6a3b55cb359d81e",
+    "url": "/agency-gsap/static/js/2.95694cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2067ee611066541443",
+    "url": "/agency-gsap/static/js/main.6a82c771.chunk.js"
+  },
+  {
+    "revision": "b182229cb7c07aaf9f4e",
+    "url": "/agency-gsap/static/js/runtime-main.5d7625d5.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/agency-gsap/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/agency-gsap/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/agency-gsap/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/agency-gsap/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/agency-gsap/static/media/up-arrow-circle.d6c4e78d.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/agency-gsap/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
